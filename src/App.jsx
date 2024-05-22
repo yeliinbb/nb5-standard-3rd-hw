0d00efbc3853fc8c551ee0d096b3d3794252c557
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-05-22 15:30:26 +0900

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ export default function App() {
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
   const filteredStudents = students.filter((student) => student.age >= 18);
 
+  const handleAlert = (student) => {
+    alert(`나이 : ${student.age}, 점수 : ${student.grade}`);
+  };
+
   return (
     <div>
       <h1>학생 목록</h1>
@@ -16,15 +20,7 @@ export default function App() {
         {
           /* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */
           filteredStudents.map((student) => {
-            return (
-              <li
-                onClick={() =>
-                  alert(`나이 : ${student.age}, 점수 : ${student.grade}`)
-                }
-              >
-                {student.name}
-              </li>
-            );
+            return <li onClick={() => handleAlert(student)}>{student.name}</li>;
           })
         }
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
